bceb03839562eee0bc17a4589dce2386a9c88a69
Felipedomazos <fe.domazos@duocuc.cl>
2022-09-03 18:19:36 -0400

    Agregado contenido ts login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user={
+    usuario:"",
+    contrasena:""
+  }
+
+  constructor(private router: Router, private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+  ingresar(){
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    };
+    this.router.navigate(['/home'],navigationExtras);
+  }
+
+  async presentToast(position: 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Formulario enviado al correo exitosamente!',
+      duration: 2000,
+      position: position
+    });
+
+    await toast.present();
   }
 
 }
