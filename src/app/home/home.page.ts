e6d332c46d055fc15cf5432faf24d923d881e2d5
Felipedomazos <fe.domazos@duocuc.cl>
2022-09-03 23:43:24 -0400

    Agregado de paso de info 'login' a 'home'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: any;
+
+  constructor(private activeroute: ActivatedRoute, private router: Router, private menu: MenuController) {
+    this.activeroute.queryParams.subscribe(params => { 
+      if (this.router.getCurrentNavigation().extras.state) { 
+        this.data = this.router.getCurrentNavigation().extras.state.user; 
+        console.log(this.data) 
+      }else{this.router.navigate(["/login"])} 
+    });
+
+  }
 
 }
