24db50bc601ee80f7536068b04d0cef3a5ceb9f5
danielgaray-19 <da.garay@duocuc.cl>
2022-09-07 01:18:50 -0300

    boton fallido

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,27 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Component } from '@angular/core';
 
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: 'example.component.html',
+})
+export class ExampleComponent {
+  constructor(private loadingCtrl: LoadingController) {}
+
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      duration: 3000,
+      spinner: 'circles',
+    });
+
+    loading.present();
+  }
+}
 
 @NgModule({
   imports: [
@@ -17,3 +37,4 @@ import { HomePageRoutingModule } from './home-routing.module';
   declarations: [HomePage]
 })
 export class HomePageModule {}
+
