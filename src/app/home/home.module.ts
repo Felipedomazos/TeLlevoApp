86c33825202e56439608f24405d9c03887b8654d
daniel campos <da.camposk@duocuc.cl>
2022-09-07 01:11:10 -0400

    arreglo bug

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, LoadingController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { Component } from '@angular/core';
 
-import { LoadingController } from '@ionic/angular';
 
-@Component({
-  selector: 'app-example',
-  templateUrl: 'example.component.html',
-})
+
+
 export class ExampleComponent {
-  constructor(private loadingCtrl: LoadingController) {}
+  constructor(private loadingCrol: LoadingController) {}
 
   async showLoading() {
-    const loading = await this.loadingCtrl.create({
+    const loading = await this.loadingCrol.create({
       message: 'Loading...',
       duration: 3000,
       spinner: 'circles',
