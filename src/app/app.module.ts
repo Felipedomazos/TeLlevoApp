ecf1d6618d68a02cf07759f3ec51a4f0629c563d
Felipedomazos <fe.domazos@duocuc.cl>
2022-11-28 11:39:08 -0300

    Configuracion Efecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { isPlatform, IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
@@ -9,12 +9,33 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { ComponentsModule } from './components/components.module';
 
 
+const getConfig = () => {
+  let config = {
+    animated: true,
+    rippleEffect: true
+  };
+
+  if (isPlatform('ios')) {
+    config = {
+      animated: true,
+      rippleEffect: true
+    }
+  }
+  if (isPlatform('android')) {
+    config = {
+      animated: false,
+      rippleEffect: false
+    }
+  }
+  return config;
+}
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: 
   [BrowserModule, 
-    IonicModule.forRoot(), 
+    IonicModule.forRoot(getConfig()), 
     AppRoutingModule, 
     IonicStorageModule.forRoot(), 
     HttpClientModule,
